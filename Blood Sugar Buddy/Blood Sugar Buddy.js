d32a22ce6cd0282f3de993a3bbd555683bed6bea
Tenecia Offutt <tdcraycroft@msn.com>
2021-07-14 00:10:01 -0400

    js not linking to html

--- Blood Sugar Buddy/Blood Sugar Buddy.js
+++ Blood Sugar Buddy/Blood Sugar Buddy.js
@@ -1,9 +1,11 @@
-const "Blood Sugar Buddy" = document.getElementById('Blood Sugar Buddy')
-const button = document.getElementsByName
+console.alert('hello world');
+
+//const "Blood Sugar Buddy" = document.getElementById('Blood Sugar Buddy')
+//const button = document.getElementsByName
 
 //----------------------------------
 //FETCH FUNCTIONS
 //----------------------------------
-fetch('world.openfoodfacts.org/api/v0/product/737628064502.json')
-    .then(response => response.json())
-    .then (data => console.log(data))
+//fetch('world.openfoodfacts.org/api/v0/product/737628064502.json')
+    //.then(response => response.json())
+    //.then (data => console.log(data))
