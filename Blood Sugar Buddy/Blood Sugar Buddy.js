03417c03700682b5d3ef7807047f61ae91a26c8a
Tenecia Offutt <tdcraycroft@msn.com>
2021-07-13 23:22:07 -0400

    Added API to JS

--- Blood Sugar Buddy/Blood Sugar Buddy.js
+++ Blood Sugar Buddy/Blood Sugar Buddy.js
@@ -1,2 +1,9 @@
 const "Blood Sugar Buddy" = document.getElementById('Blood Sugar Buddy')
 const button = document.getElementsByName
+
+//----------------------------------
+//FETCH FUNCTIONS
+//----------------------------------
+fetch('world.openfoodfacts.org/api/v0/product/737628064502.json')
+    .then(response => response.json())
+    .then (data => console.log(data))
--- Blood Sugar Buddy/README.md
+++ Blood Sugar Buddy/README.md
@@ -1,2 +1,4 @@
 Blood Sugar Buddy is a JavaScript app created to assist persons that have diabetes.
 It allows the user to enter their blood sugar reading, and it will pull up a suggested menu of what to eat.
+
+07/13/2021 FETCH added to JS file
