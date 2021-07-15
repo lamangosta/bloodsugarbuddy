afcef7a6f9ce031ecce5a5727082897fd7c584f1
Tenecia Offutt <tdcraycroft@msn.com>
2021-07-14 23:24:04 -0400

    JS appears to be working, added buttons to page-one red, one blue to change color on click

--- Blood Sugar Buddy/Blood Sugar Buddy.js
+++ Blood Sugar Buddy/Blood Sugar Buddy.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+//const "Blood Sugar Buddy" = document.getElementById('Blood Sugar Buddy')
+//const button = document.getElementsByName
+ 
+//----------------------------------
+//FETCH FUNCTIONS
+//----------------------------------
+//fetch('world.openfoodfacts.org/api/v0/product/737628064502.json')
+   //.then(response => response.json())
+   //.then (data => console.log(data))
+
+var button = document.getElementById("button")
+var button = document.getElementById()
+
+function changeColor(red) {
+    var button = document.getElementById('button');
+    button.style.color = blue;
+  }
+
+  function changeColor(blue) {
+    var button = document.getElementById('button');
+    button.style.color = red;
+  }
+
+
+  
